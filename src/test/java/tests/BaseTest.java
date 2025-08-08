44338d9fb75a5bb85cd511fea9e7cd5246232c51
Dan <daniel.voda@ext.evozon.com>
2025-08-08 14:25:30 +0300

    add basetest class

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,31 @@
 package tests;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
 
 public class BaseTest {
+
+    private Playwright playwright;
+    private Browser browser;
+    protected Page page;
+
+
+    @BeforeEach
+    public void startSession () {
+        this.playwright = Playwright.create();
+        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(300));
+        this.page = browser.newPage();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        browser.close();
+        playwright.close();
+    }
 
 }
