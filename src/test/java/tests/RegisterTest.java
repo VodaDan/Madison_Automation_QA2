16004dbd3e697bfa358698fa4f1a3a58dbcbe4c5
Dan <daniel.voda@ext.evozon.com>
2025-08-08 17:01:19 +0300

    created RegisterTests class

--- src/test/java/tests/RegisterTest.java
+++ src/test/java/tests/RegisterTest.java
@@ -1,7 +1,12 @@
 package tests;
 
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pages.RegisterPage;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class RegisterTest extends BaseTest{
 
@@ -14,5 +19,17 @@ public class RegisterTest extends BaseTest{
         registerPage = new RegisterPage(page);
     }
 
+    @Test
+    public void registerVerifyInputFieldAndButtonTest(){
+        navigation.navigateToRegisterPage();
+        assertThat(page.locator(registerPage.getFirstNameSelector())).isEditable();
+        assertThat(page.locator(registerPage.getLastNameSelector())).isEditable();
+        assertThat(page.locator(registerPage.getEmailSelector())).isEditable();
+        assertThat(page.locator(registerPage.getPasswordSelector())).isEditable();
+        assertThat(page.locator(registerPage.getConfirmationPasswordSelector())).isEditable();
+        assertThat(page.locator(registerPage.getMiddleNameSelector())).isEditable();
+        assertThat(page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Register"))).isVisible();
+        assertThat(page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Register"))).isEnabled();
 
+    }
 }
