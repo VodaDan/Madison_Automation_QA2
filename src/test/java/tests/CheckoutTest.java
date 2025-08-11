97f74209fbf26dd7c4177ae89c9c1844ff548a09
Dan <daniel.voda@ext.evozon.com>
2025-08-11 15:31:17 +0300

    add test for filling billing address and continuing to shipping method - test fails atm

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -2,9 +2,13 @@ package tests;
 
 import models.DeliveryAddress;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pages.CheckoutPage;
 import pages.ProductPage;
+import pages.ShoppingCart;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class CheckoutTest extends BaseTest {
 
@@ -17,14 +21,20 @@ public class CheckoutTest extends BaseTest {
         checkoutPage = new CheckoutPage(page);
     }
 
+    @Disabled("Test fails - does not follow the shipping page after.")
     @Test
-    public void checkoutFinishOrder() {
+    public void checkoutBillingAddressOrder() {
         DeliveryAddress address = new DeliveryAddress();
         ProductPage productPage = new ProductPage(page);
+        ShoppingCart shoppingCartPage = new ShoppingCart(page);
         navigation.navigateToHomepage();
+        productPage.addRandomProductToCart();
+        shoppingCartPage.clickProceedToCheckout();
+        checkoutPage.checkoutClickOnePageContinueButton();
         checkoutPage.fillCheckoutForm(address);
         checkoutPage.fillRegion(); // default is "Alaska"
         checkoutPage.continueCheckout();
+        assertThat(page.locator("li#opc-shipping")).containsClass("active");
     }
 
 }
