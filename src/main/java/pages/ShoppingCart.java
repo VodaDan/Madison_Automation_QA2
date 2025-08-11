453c92018bbb422326e7689ad48d3246b491262e
Dan <daniel.voda@ext.evozon.com>
2025-08-11 15:28:42 +0300

    add proceedToCheckoutButton for proceeding to order finish

--- src/main/java/pages/ShoppingCart.java
+++ src/main/java/pages/ShoppingCart.java
@@ -6,11 +6,13 @@ public class ShoppingCart {
     private Page page;
     private String quantitySelector;
     private String removeItemButtonSelector;
+    private String proceedToCheckoutButton;
 
     public ShoppingCart(Page pageSent){
         page = pageSent;
         quantitySelector         = "#qty";
         removeItemButtonSelector = "tr.first td.a-center a.btn-remove";
+        proceedToCheckoutButton = "li.method-checkout-cart-methods-onepage-bottom button";
     }
 
     public void setQuantity(String quantity) {
@@ -19,6 +21,10 @@ public class ShoppingCart {
 
     public void removeItemFromCart() {
         page.locator(removeItemButtonSelector).click();
+    }
+
+    public void clickProceedToCheckout() {
+        page.locator(proceedToCheckoutButton).click();
     }
 }
 
--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,7 +1,10 @@
 package tests;
 
+import models.DeliveryAddress;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pages.CheckoutPage;
+import pages.ProductPage;
 
 public class CheckoutTest extends BaseTest {
 
@@ -12,6 +15,16 @@ public class CheckoutTest extends BaseTest {
     public void startSession () {
         super.startSession();
         checkoutPage = new CheckoutPage(page);
+    }
+
+    @Test
+    public void checkoutFinishOrder() {
+        DeliveryAddress address = new DeliveryAddress();
+        ProductPage productPage = new ProductPage(page);
+        navigation.navigateToHomepage();
+        checkoutPage.fillCheckoutForm(address);
+        checkoutPage.fillRegion(); // default is "Alaska"
+        checkoutPage.continueCheckout();
     }
 
 }
